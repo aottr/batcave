0ac6a34fbe62d7569d57f320c3a85c98034f458d
A.Ottr <alex@tailbyte.org>
2023-04-24 21:43:02 +0200

    add cart functionality

--- storefront/urls.py
+++ storefront/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ProductView, CategoryView
+from .views import ProductView, CategoryView, add_to_cart
 
 urlpatterns = [
     path('', CategoryView.as_view(), name='category-detail'),
     path('pp/<slug:slug>', ProductView.as_view(), name='product-detail'),
+    path('pp/<slug:slug>/add', add_to_cart, name='add-to-cart'),
 ]
--- storefront/views.py
+++ storefront/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, DetailView, ListView
-from storefront.models import Product
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import FormMixin
+from django.urls import reverse
+from urllib.parse import urlencode
+from storefront.models import Product, CartItem
 
 
 class CategoryView(ListView):
@@ -10,3 +14,13 @@ class CategoryView(ListView):
 
 class ProductView(DetailView):
     model = Product
+
+
+def add_to_cart(request, slug):
+    # TODO Error handling
+    product = Product.objects.get(slug=slug)
+    request.user.cart.items.add(CartItem.objects.create(item=product))
+    request.user.cart.total += product.base_price
+    request.user.cart.save()
+
+    return redirect('product-detail', slug=slug)
